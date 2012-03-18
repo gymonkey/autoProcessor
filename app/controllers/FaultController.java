42a29b4c1806cbcc34426dd5a8ceb73d9a5be960
gymonkey <lee.gymonkey@gmail.com>
2012-03-18 15:29:18 +0800

    完成故障查看骨架内容

---
+++ app/controllers/FaultController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import models.App;
+import models.Fault;
+
+import play.mvc.Controller;
+
+public class FaultController extends Controller{
+
+	public static void index(){
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_YEAR, -1);
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		String queryDate = format.format(cal.getTime());
+		
+		List<App> appList = App.getAllApp();
+		
+		render(appList, queryDate);
+	}
+	
+	public static void viewFaultData(long appID, String queryData, String type, int page){
+		List<Fault> list = null;
+		
+		if(type.equalsIgnoreCase("ALL_TYPE")){
+			list = Fault.getFalut(appID, queryData, page);
+		}else{
+			list = Fault.getFault(appID, queryData, type, page);
+		}
+		
+		renderTemplate("FaultController/faultData.html", list);
+	}
+}
